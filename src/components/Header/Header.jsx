7456999461442bf67930cf6a9be5258a746550bf
napatsawan-az <napatsawan.az@gmail.com>
2024-05-27 16:51:51 +0700

    add account on navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,23 @@ const Header = () => {
     <>
       <header className="block sticky top-0 bg-stbGray h-[72px] py-3 w-full z-20">
         <nav className="max-w-[1080px] mx-auto h-12 px-3">
-          <ul>
-            <li className="">
+          <ul className="flex justify-between">
+            <li>
               <Link to="/">
                 <img src="/logo.png" alt="Logo" className="w-12 h-12" />
               </Link>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <p className="hidden md:block mr-3 text-white">
+                  Napatsawan Armin
+                </p>
+                <img
+                  src="./src/assets/account.svg"
+                  alt="account"
+                  className="w-12 h-12"
+                />
+              </div>
             </li>
           </ul>
         </nav>
