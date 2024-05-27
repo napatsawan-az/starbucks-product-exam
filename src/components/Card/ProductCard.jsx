c50a89c1fd88b4d501fcc0a329837be5cc4d75f0
napatsawan-az <napatsawan.az@gmail.com>
2024-05-27 15:53:41 +0700

    fix card style

--- src/components/Card/ProductCard.jsx
+++ src/components/Card/ProductCard.jsx
@@ -1,18 +1,20 @@
 const ProductCard = ({ product }) => {
   return (
-    <div className="card w-full bg-base-100 rounded-none">
+    <div className="card w-full h-full bg-base-100 rounded-none shadow-md">
       <figure>
         <img src={product.image_url} alt={product.name} />
       </figure>
       <div className="card-body p-0">
-        <h2 className="card-title mt-2 font-bold text-base">{product.name}</h2>
+        <h2 className="card-title mt-2 font-bold text-base ml-2">
+          {product.name}
+        </h2>
         <div className="card-actions mb-6">
-          <div className="badge badge-neutral font-bold text-xs">
+          <div className="badge badge-neutral font-bold text-xs ml-2">
             {product.category}
           </div>
         </div>
-        <p>{product.price} Baht</p>
       </div>
+      <p className="ml-2 mb-2">{product.price} Baht</p>
     </div>
   );
 };
